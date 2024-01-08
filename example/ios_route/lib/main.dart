ac652fc84d8ae84aac85c41ba8b31567e8618eb1
Jacob  Moura <jacobaraujo7@gmail.com>
2024-01-07 21:09:25 -0300

    update example

--- example/ios_route/lib/main.dart
+++ example/ios_route/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'app/app_widget.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
+
   runApp(const AppWidget());
 }
