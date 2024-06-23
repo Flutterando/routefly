8e4ebe5798dcd5b1a3df9675fbaf8997379d7859
Maurício Sousa <morricedev@gmail.com>
2024-06-23 17:24:26 -0300

    feat: add flag to change app dir path

--- bin/routefly.dart
+++ bin/routefly.dart
@@ -3,20 +3,21 @@ import 'dart:io';
 import 'package:routefly/src/usecases/generate_routes.dart';
 
 Future<void> main(List<String> args) async {
-  final flag = args.isEmpty ? '--generate' : args.first;
+  final flags = _listStringToMap(args);
+  final runMode = _getRunMode(flags);
 
-  final appDir = Directory('./lib/app');
+  final appDir = Directory(flags['app-dir'] ?? './lib/app');
   final routeFile = File('${appDir.parent.path}/routes.g.dart');
 
   final generate = GenerateRoutes(appDir, routeFile);
 
   Stream<ConsoleResponse>? stream;
 
-  if (flag == '--generate') {
+  if (runMode == '--generate') {
     stream = generate.call();
-  } else if (flag == '--watch') {
+  } else if (runMode == '--watch') {
     stream = _startWatch(generate, appDir);
-  } else if (flag == '--init') {
+  } else if (runMode == '--init') {
     _init(generate, appDir);
     exit(0);
   }
@@ -26,6 +27,28 @@ Future<void> main(List<String> args) async {
       c.log();
     }
   }
+}
+
+Map<String, dynamic> _listStringToMap(List<String> list) {
+  final map = <String, dynamic>{};
+  for (var i = 0; i < list.length; i++) {
+    if (list[i].startsWith('--')) {
+      if (!list[i].contains('=')) {
+        map[list[i].substring(2)] = true;
+        continue;
+      }
+      final key = list[i].substring(2, list[i].indexOf('='));
+      final value = list[i].substring(list[i].indexOf('=') + 1);
+      map[key] = value;
+    }
+  }
+  return map;
+}
+
+String _getRunMode(Map<String, dynamic> flags) {
+  if (flags['watch'] == true) return '--watch';
+  if (flags['init'] == true) return '--init';
+  return '--generate';
 }
 
 Stream<ConsoleResponse> _startWatch(
